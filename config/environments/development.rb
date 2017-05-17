01aa5ebc09f66af84878bd2fe74c61d04a243664
SrdjanPolic <srdjan.polic@yahoo.com>
2017-05-17 23:56:31 +0200

    adrese

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,6 +49,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
