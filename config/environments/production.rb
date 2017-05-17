71ff5e1e686aa2b692a77b94d04d3c2759db0fd3
SrdjanPolic <srdjan.polic@yahoo.com>
2017-05-17 23:31:49 +0200

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,17 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.action_mailer.default_url_options = { :host => 'https://warm-peak-66994.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: "velikimret74@gmail.com",
+      password: "mretenizam"
+  }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
